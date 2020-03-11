0b25074a18c955dd64b2f2c4637d438fc5f71c5c
ClAndrew <cmlandrews@gmail.com>
2020-03-11 13:39:00 -0700

    Fix visibility of the admin bar

--- theme/waterlily/footer.php
+++ theme/waterlily/footer.php
@@ -1,6 +1,13 @@
 <div class="footertext">
-<?php bloginfo('name'); ?> <br /><a href="feed:<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a> and <a href="feed:<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a>.<br />
-
-Based off of a theme design by <a href="https://lisasabin-wilson.com">Lisa Sabin</a> with some other stuff added.
-
+<?php bloginfo('name'); ?>
+<br />
+<a href="feed:<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a> and <a href="feed:<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a>.<br />
+<?php wp_footer(); ?>
 </div>
+
+<?php
+function print_footer()
+{
+	echo "Based off of a theme design by <a href=\"https://lisasabin-wilson.com\">Lisa Sabin</a> with some other stuff added.";
+}
+?>

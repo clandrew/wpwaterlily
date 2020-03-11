3b50b053272cc4b740e0fb6246153b40829f4f95
ClAndrew <cmlandrews@gmail.com>
2020-03-11 08:30:58 -0700

    Rejig style and add actual theme credit

--- theme/waterlily/footer.php
+++ theme/waterlily/footer.php
@@ -1,10 +1,6 @@
 <div class="footertext">
 <?php bloginfo('name'); ?> <br /><a href="feed:<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a> and <a href="feed:<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a>.<br />
 
-
-
-Based off of a theme design by Lisa Sabin and <?php wp_footer(); ?>
-
-
+Based off of a theme design by <a href="https://lisasabin-wilson.com">Lisa Sabin</a> with some other stuff added.
 
 </div>

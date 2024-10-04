8ef08c1bd34ad6b024b84447288408fccab882bf
clandrew <cmlandrews@gmail.com>
2024-10-03 18:54:40 -0700

    Whitespace changes

--- theme/waterlily/page.php
+++ theme/waterlily/page.php
@@ -1,38 +1,38 @@
-<?php get_header(); ?>
-
-<div id="main">
-
-<!-- end header -->
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-	
-<div class="main_title"><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></div>
-
-<div class="main_post">
-
-<?php the_content(__('<strong>&raquo; Continue Reading</strong>')); ?></div>
-
-<!--
-	<?php trackback_rdf(); ?>
-	-->
-
-
-
-<?php endwhile; else: ?>
-<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
-<?php endif; ?>
-
-
-</div> 
-
-
-<div id="menu">
-<?php get_sidebar(); ?>
-</div>
-
-</div>
-<div class="clearfix"></div>
-<div id="footer"><?php get_footer(); ?></div>
-</div>
-
-</body>
+<?php get_header(); ?>
+
+<div id="main">
+
+<!-- end header -->
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+	
+<div class="main_title"><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></div>
+
+<div class="main_post">
+
+<?php the_content(__('<strong>&raquo; Continue Reading</strong>')); ?></div>
+
+<!--
+	<?php trackback_rdf(); ?>
+	-->
+
+
+
+<?php endwhile; else: ?>
+<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+<?php endif; ?>
+
+
+</div> 
+
+
+<div id="menu">
+<?php get_sidebar(); ?>
+</div>
+
+</div>
+<div class="clearfix"></div>
+<div id="footer"><?php get_footer(); ?></div>
+</div>
+
+</body>
 </html>
